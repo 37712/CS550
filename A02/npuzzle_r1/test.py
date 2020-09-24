68f9a4ab678b8feaa752cf6a603a12854b4c9b0a
37712 <cgr37712@gmail.com>
2020-09-24 15:49:17 -0700

    test.py is now a test driver to test with one one search

--- A02/npuzzle_r1/test.py
+++ A02/npuzzle_r1/test.py
@@ -1,82 +1,52 @@
-from basicsearch_lib02.timer import Timer
-import time
+'''
+driver for graph search problem
+
+'''
+
 from statistics import (mean, stdev)  # used for printing summary
 
-class myClass(object):
-    def __init__(self):
-        "__init__() - Create an empty explored set"
-        # we should use dict or list or set to store this info
-        self.explored_set = dict()
+from npuzzle import NPuzzle
+from basicsearch_lib02.tileboard import TileBoard
+from basicsearch_lib02.timer import Timer
+from searchstrategies import (BreadthFirst, DepthFirst, Manhattan)
+from problemsearch import graph_search
+import collections
 
-    def exists(self, state):
-        """
-        exists(state) - Has this state already been explored?
-        :param state:  Hashable problem state
-        :return: True if already seen, False otherwise4
-        """
-        return hash(state) in self.explored_set
+numPuzzles = 1 #31 # number of puzzles
+puzzleSize = 8 # size of puzzel, 8 will give us a 3x3 grid puzzle
+searchType = [BreadthFirst, DepthFirst, Manhattan] # list of search methods
 
-    def add(self, state):
-        """
-        add(state) - Add a given state to the explored set
-        :param state:  A problem state that is hashable, e.g. a tuple
-        :return: None
-        """
-        # we use the hash as the index to save the state
-        if(not self.exists(state)):
-            self.explored_set[hash(state)] = state
 
-def foo():
-    return "hello", 123, "world"
+#used for configuration purposes
+debug = False
+vervose = False
 
-def doo(x = False):
-    return x
+def driver() :
 
-def eoo(**kwargs): # basically a dictionary
-    return kwargs['q']
+    lengthOfPath = list()
+    numOfNodes = list()
+    timeElapsedSec = list()
 
-def goo(a,b,c):
-    print(a)
-    print(b)
-    print(c)
+    # PROCESSING 
+    search = BreadthFirst
 
-"""
-#######################################TEST#######################################
-"""
+    # create the puzzle
+    npuzzle = NPuzzle(puzzleSize, g = search.g, h = search.h)
 
-def driver():
-    """
-    x,y,z =foo()
-    print(x,y,z)
+    # solving the npuzzle
+    # graph_search will return 3 values, the path, nodes explored, and elapsed time in seconds
+    path, explored, elapsedtime = graph_search(npuzzle, debug=False, verbose=False)
 
-    print(doo())
-    print(doo(100))
+    print("puzzle solved in %dmin %dsec"
+        % (int(elapsedtime/60), elapsedtime%60))
 
-    print(eoo(a ="adsfasdf", o = 3245342, q = "hello master", z = 12431234))
+    # appending the lenth of the path to the list
+    lengthOfPath.append(len(path))
 
-    goo(c = 111, a = 222, b = 333)
-        
-    x = myClass()
-    x.add("hello")
-    x.exists("hello")
-    
-    t = Timer()
-    while(True):
-        print("%dmin %dsec" % (int(t.elapsed_s()/60), t.elapsed_s()%60))
-        time.sleep(1)
-    
-    path = None
-    assert path is not None
-    """
+    # appending number of nodes explored
+    numOfNodes.append(explored)
 
-    x = list()
-    x.append(8)
-    x.append(6)
-    x.append(4)
-    x.append(1)
-    x.append(7)
-    print(x)
-    print(mean(x))
-    print(stdev(x))
+    # appending the time it took to solve
+    timeElapsedSec.append(elapsedtime)
 
 driver()
